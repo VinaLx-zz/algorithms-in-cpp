db13d5bb3fe9f9ea2052ec8dda7a442c842ab678
VinaLx <vinaleux@gmail.com>
2017-02-10 22:31:10 +0800

    avl debugged

--- src/tree/AvlTree.hpp
+++ src/tree/AvlTree.hpp
@@ -2,7 +2,6 @@
 #define AVL_TREE_HPP_
 
 #include <algorithm>
-#include "BinarySearchTree.hpp"
 
 namespace vinalx {
 
@@ -11,9 +10,11 @@ namespace tree {
 template <typename Key>
 class AvlTree {
   public:
-    struct Node : BinaryNodeBase<Key> {
-        Node(const Key& data, Node* left, Node* right)
-            : BinaryNodeBase<Key>(data, left, right) {}
+    struct Node {
+        Node(const Key& data, Node* left = nullptr, Node* right = nullptr)
+            : left(left), right(right), data(data) {}
+        Node *left, *right;
+        Key data;
         int left_height = 1;
         int right_height = 1;
     };
@@ -21,9 +22,120 @@ class AvlTree {
     void Insert(const Key& key) {
         root_ = Insert(key, root_);
     }
-    void Erase(const Key& key);
+
+    void Erase(const Key& key) {
+        root_ = Erase(root_, key);
+    }
+
+    size_t Size() const {
+        return size_;
+    }
+
+    bool Has(const Key& key) const {
+        return Has(root_, key);
+    }
+
+    template <typename Func>
+    void InorderTraverse(Func f) const {
+        InorderTraverse(root_, f);
+    }
 
   private:
+    bool Has(Node* node, const Key& key) const {
+        if (node == nullptr) {
+            return false;
+        }
+        if (key < node->data) {
+            return Has(node->left, key);
+        }
+        if (key > node->data) {
+            return Has(node->right, key);
+        }
+        return true;
+    }
+
+    template <typename Func>
+    void InorderTraverse(Node* node, Func f) const {
+        if (node == nullptr) {
+            return;
+        }
+        InorderTraverse(node->left, f);
+        f(node->data);
+        InorderTraverse(node->right, f);
+    }
+
+    Node* Insert(const Key& key, Node* node) {
+        if (node == nullptr) {
+            ++size_;
+            return new Node(key);
+        }
+        if (key < node->data) {
+            node->left = Insert(key, node->left);
+        } else if (key > node->data) {
+            node->right = Insert(key, node->right);
+        } else {
+            return node;
+        }
+        return Rebalance(node);
+    }
+
+    Node* Erase(Node* node, const Key& key) {
+        if (node == nullptr) {
+            return nullptr;
+        }
+        if (key < node->data) {
+            node->left = Erase(node->left, key);
+            return Rebalance(node);
+        } else if (key > node->data) {
+            node->right = Erase(node->right, key);
+            return Rebalance(node);
+        }
+
+        Node* temp = node;
+
+        if (node->left == nullptr) {
+            node = node->right;
+        } else if (node->right == nullptr) {
+            node = node->left;
+        } else if (node->left->right == nullptr) {
+            node->left->right = node->right;
+            node->left->right_height = HeightOf(node->right) + 1;
+            node = node->left;
+        } else {
+            Node* max_in_left = nullptr;
+            node->left = GetMax(node->left, &max_in_left);
+            max_in_left->left = node->left;
+            max_in_left->right = node->right;
+            node = max_in_left;
+        }
+        delete temp;
+        --size_;
+        return Rebalance(node);
+    }
+
+    Node* Rebalance(Node* node) {
+        if (node == nullptr) {
+            return nullptr;
+        }
+        node->left_height = HeightOf(node->left) + 1;
+        node->right_height = HeightOf(node->right) + 1;
+        if (node->left_height - node->right_height > 1) {
+            if (node->left->right_height > node->left->left_height) {
+                return DoubleRotateRight(node);
+            } else {
+                return RotateRight(node);
+            }
+        }
+        if (node->right_height - node->left_height > 1) {
+            if (node->right->left_height > node->right->right_height) {
+                return DoubleRotateLeft(node);
+            } else {
+                return RotateLeft(node);
+            }
+        }
+        return node;
+    }
+
     int HeightOf(Node* node) const {
         if (node == nullptr) {
             return 0;
@@ -65,36 +177,17 @@ class AvlTree {
         return RotateRight(node);
     }
 
-    Node* Insert(const Key& key, Node* node) {
-        if (node == nullptr) {
-            ++size_;
-            return new Node(key);
+    Node* GetMax(Node* node, Node** result) {
+        if (node->right == nullptr) {
+            *result = node;
+            return node->left;
         }
-        if (key < node->data) {
-            node->left = Insert(key, node->left);
-        } else if (key > node->data) {
-            node->right = Insert(key, node->right);
-        } else {
-            return node;
-        }
-        if (node->left_height - node->right_height > 1) {
-            if (node->left->right_height > node->left->left_height) {
-                return DoubleRotateRight(node);
-            } else {
-                return RotateRight(node);
-            }
-        } else if (node->right_height - node->left_height > 1) {
-            if (node->right->left_height > node->right->right_height) {
-                return DoubleRotateLeft(node);
-            } else {
-                return RotateLeft(node);
-            }
-        }
-        return node;
+        node->right = GetMax(node->right, result);
+        return Rebalance(node);
     }
 
-        Node* root_ = nullptr;
-        size_t size_ = 0;
+    Node* root_ = nullptr;
+    size_t size_ = 0;
 };
 
 }  // namespace tree
--- test/tree/test_bst.cpp
+++ test/tree/test_bst.cpp
@@ -3,50 +3,61 @@
 #include <iostream>
 #include <random>
 #include <vector>
+#include "tree/AvlTree.hpp"
 #include "tree/BinarySearchTree.hpp"
 
 using vinalx::tree::BinarySearchTree;
+using vinalx::tree::AvlTree;
 using std::vector;
 
-template <typename T>
-vector<T> Traverse(const BinarySearchTree<T>& bst) {
+template <typename T, template <typename> class Tree>
+vector<T> Traverse(const Tree<T>& bst) {
     vector<T> result;
     bst.InorderTraverse([&result](const T& data) { result.push_back(data); });
     return result;
 }
 
-template <typename T>
-bool CheckSorted(const BinarySearchTree<T>& bst) {
+template <typename T, template <typename> class Tree>
+bool CheckSorted(const Tree<T>& bst) {
     vector<T> traverse_result = Traverse(bst);
     return std::is_sorted(begin(traverse_result), end(traverse_result));
 }
 
-template <typename T, typename Container>
-BinarySearchTree<T> InitWith(const Container& cont) {
-    BinarySearchTree<T> bst;
+template <typename Tree, typename Container>
+Tree InitWith(const Container& cont) {
+    Tree bst;
     for (const auto& v : cont) {
         bst.Insert(v);
     }
     return bst;
 }
 
-TEST(BstTest, SimpleInsertTest) {
-    BinarySearchTree<int> bst;
-    EXPECT_EQ(bst.Size(), 0);
+template <typename Bst>
+class BstTest : public testing::Test {};
+
+using TreeTypes = testing::Types<BinarySearchTree<int>, AvlTree<int>>;
+
+TYPED_TEST_CASE(BstTest, TreeTypes);
+
+TYPED_TEST(BstTest, SimpleInsertTest) {
+    TypeParam bst;
+    ASSERT_EQ(bst.Size(), 0);
     bst.Insert(1);
-    EXPECT_EQ(bst.Size(), 1);
+    ASSERT_EQ(bst.Size(), 1);
+    ASSERT_TRUE(bst.Has(1)) << "After insert 1";
     bst.Insert(2);
-    EXPECT_EQ(bst.Size(), 2);
-    EXPECT_TRUE(bst.Has(2));
-    EXPECT_TRUE(bst.Has(1));
-    EXPECT_TRUE(CheckSorted(bst));
+    ASSERT_EQ(bst.Size(), 2);
+    ASSERT_TRUE(bst.Has(2)) << "After insert 2";
+    ASSERT_TRUE(bst.Has(1)) << "After insert 2";
+    ASSERT_TRUE(CheckSorted(bst));
 }
 
-TEST(BstTest, InsertTest) {
+TYPED_TEST(BstTest, InsertTest) {
     vector<int> values{6, 4, 8, 9, 1, 0, 3, 5, 7, 2};
-    BinarySearchTree<int> bst;
+    TypeParam bst;
     for (auto v : values) {
         bst.Insert(v);
+        EXPECT_TRUE(bst.Has(v));
     }
     for (auto v : values) {
         EXPECT_TRUE(bst.Has(v));
@@ -54,9 +65,9 @@ TEST(BstTest, InsertTest) {
     EXPECT_TRUE(CheckSorted(bst));
 }
 
-TEST(BstTest, EraseTest) {
+TYPED_TEST(BstTest, EraseTest) {
     vector<int> values{6, 4, 8, 9, 1, 0, 3, 5, 7, 2};
-    auto bst = InitWith<int>(values);
+    auto bst = InitWith<TypeParam>(values);
     int size = bst.Size();
     EXPECT_EQ(size, 10);
     for (auto v : values) {
@@ -69,9 +80,9 @@ TEST(BstTest, EraseTest) {
     EXPECT_EQ(bst.Size(), 0);
 }
 
-TEST(BstTest, EraseTest2) {
+TYPED_TEST(BstTest, EraseTest2) {
     vector<int> values{1, 4, 7, 2, 5, 8, 9, 0, 10, 6, 13};
-    auto bst = InitWith<int>(values);
+    auto bst = InitWith<TypeParam>(values);
     ASSERT_EQ(bst.Size(), 11);
     int size = bst.Size();
     for (auto v : values) {
